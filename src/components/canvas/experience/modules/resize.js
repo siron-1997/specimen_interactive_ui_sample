1fac968daff31b7de929e8a6d01cac8b33a19d83
siron-1997 <sironvssiron@gmail.com>
2022-05-08 14:03:52 +0900

    ウィンドウリサイズバグ除去

--- src/components/canvas/experience/modules/resize.js
+++ src/components/canvas/experience/modules/resize.js
@@ -2,10 +2,10 @@ export default function resize (sizes, camera, renderer) {
     window.addEventListener('resize', () => {
         sizes.width = window.innerWidth
         sizes.height = window.innerHeight
-        sizes.height = window.devicePixelRatio
+        sizes.pixelRatio = window.devicePixelRatio
         camera.aspect = sizes.width / sizes.height
         camera.updateProjectionMatrix()
         renderer.setSize(sizes.width, sizes.height)
-        renderer.setPixelRatio(sizes.pixelRatio)
+        renderer.setPixelRatio(Math.min(sizes.pixelRatio, 2))
     })
 }
